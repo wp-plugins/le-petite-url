78307aa8f92ada2b0d90059164ac6b01672fb113
philnelson <philnelson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-24 20:53:12 +0000

    Version 1.5: Custom short domains, shortlink support.
    git-svn-id: https://plugins.svn.wordpress.org/le-petite-url/trunk@148717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- la-petite-url.php
+++ la-petite-url.php
@@ -28,10 +28,10 @@ global $petite_table;
 
 $petite_table = "le_petite_urls";
 
-add_option("le_petite_url_version", "1.06");
+add_option("le_petite_url_version", "1.5");
 add_option("le_petite_url_use_mobile_style", "yes");
 add_option("le_petite_url_link_text", "petite url");
-add_option("le_petite_url_permalink_prefix", "");
+add_option("le_petite_url_permalink_prefix", "default");
 add_option("le_petite_url_permalink_custom", "/a/");
 add_option("le_petite_url_use_lowercase", "yes");
 add_option("le_petite_url_use_uppercase", "no");
@@ -41,15 +41,17 @@ add_option("le_petite_use_short_url", "yes");
 add_option("le_petite_url_registered", "no");
 add_option("le_petite_url_registered_on", "0");
 add_option("extra_future_site_id",md5(get_bloginfo('url')));
+add_option("le_petite_use_shortlink", "yes");
+add_option("le_petite_url_permalink_domain", "default");
+add_option("le_petite_url_domain_custom", "");
+add_option("le_petite_url_hide_godaddy","no");
 
 function le_petite_url_check_url($the_petite)
 {
 	global $wpdb;
 	global $petite_table;
 
-	//echo $the_petite;
 	$post_query = $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."$petite_table WHERE petite_url = '".$the_petite."'");
-	//echo "SELECT * FROM ".$wpdb->prefix."$petite_table WHERE petite_url = '".$the_petite."'";
 	if(count($post_query) > 0)
 	{
 		return true;
@@ -182,7 +184,19 @@ function le_petite_url_generate_sidebar()
 	global $wp_query;
 	global $wpdb;
 	global $petite_table;
-	$blogurl = get_bloginfo('siteurl');
+	
+	$le_petite_url_permalink_domain = get_option('le_petite_url_permalink_domain');
+	$le_petite_url_domain_custom = get_option('le_petite_url_domain_custom');
+	
+	if($le_petite_url_permalink_domain != "custom" )
+	{
+		$blogurl = get_bloginfo('siteurl');
+	}
+	else
+	{
+		$blogurl = 'http://'.$le_petite_url_domain_custom;
+	}
+	
 	$url_table = $wpdb->prefix . $petite_table;
 	$post_id = $wpdb->escape($_GET['post']);
 	
@@ -191,7 +205,7 @@ function le_petite_url_generate_sidebar()
 	if($petite_url != "")
 	{
 		$le_petite_url_permalink = $blogurl;
-		if(get_option('le_petite_url_permalink_prefix') != "")
+		if(get_option('le_petite_url_permalink_prefix') == "custom")
 		{
 			$le_petite_url_permalink = $le_petite_url_permalink . get_option('le_petite_url_permalink_custom');
 		}
@@ -214,7 +228,19 @@ function the_petite_url()
 	global $wp_query;
 	global $wpdb;
 	global $petite_table;
-	$blogurl = get_bloginfo('siteurl');
+	
+	$le_petite_url_permalink_domain = get_option('le_petite_url_permalink_domain');
+	$le_petite_url_domain_custom = get_option('le_petite_url_domain_custom');
+	
+	if($le_petite_url_permalink_domain != "custom" )
+	{
+		$blogurl = get_bloginfo('siteurl');
+	}
+	else
+	{
+		$blogurl = 'http://'.$le_petite_url_domain_custom;
+	}
+	
 	$url_table = $wpdb->prefix . $petite_table;
 	$post_id = $wp_query->post->ID;
 	
@@ -245,8 +271,18 @@ function the_petite_url_link()
 	global $wp_query;
 	global $wpdb;
 	global $petite_table;
+	$le_petite_url_permalink_domain = get_option('le_petite_url_permalink_domain');
+	$le_petite_url_domain_custom = get_option('le_petite_url_domain_custom');
 	
-	$blogurl = get_bloginfo('siteurl');
+	if($le_petite_url_permalink_domain != "custom" )
+	{
+		$blogurl = get_bloginfo('siteurl');
+	}
+	else
+	{
+		$blogurl = 'http://'.$le_petite_url_domain_custom;
+	}
+	
 	$url_table = $wpdb->prefix . $petite_table;
 	$post_id = $wp_query->post->ID;
 	$anchor_text = get_option('le_petite_url_link_text');
@@ -255,7 +291,7 @@ function the_petite_url_link()
 	if($petite_url != "")
 	{
 		$le_petite_url_permalink = $blogurl;
-		if(get_option('le_petite_url_permalink_prefix') != "")
+		if(get_option('le_petite_url_permalink_prefix') == "custom")
 		{
 			$le_petite_url_permalink = $le_petite_url_permalink . get_option('le_petite_url_permalink_custom');
 		}
@@ -291,7 +327,18 @@ function le_petite_url_short_url_header()
 		global $wpdb;
 		global $petite_table;
 		
-		$blogurl = get_bloginfo('siteurl');
+		$le_petite_url_permalink_domain = get_option('le_petite_url_permalink_domain');
+		$le_petite_url_domain_custom = get_option('le_petite_url_domain_custom');
+		
+		if($le_petite_url_permalink_domain != "custom" )
+		{
+			$blogurl = get_bloginfo('siteurl');
+		}
+		else
+		{
+			$blogurl = 'http://'.$le_petite_url_domain_custom;
+		}
+		
 		$url_table = $wpdb->prefix . $petite_table;
 		$post_id = $wp_query->post->ID;
 		$anchor_text = get_option('le_petite_url_link_text');
@@ -300,7 +347,7 @@ function le_petite_url_short_url_header()
 		if($petite_url != "")
 		{
 			$le_petite_url_permalink = $blogurl;
-			if(get_option('le_petite_url_permalink_prefix') != "")
+			if(get_option('le_petite_url_permalink_prefix') == "custom")
 			{
 				$le_petite_url_permalink = $le_petite_url_permalink . get_option('le_petite_url_permalink_custom');
 			}
@@ -311,6 +358,51 @@ function le_petite_url_short_url_header()
 			$le_petite_url_permalink = $le_petite_url_permalink . $petite_url;
 			
 			echo '<link rel="shorturl" href="'.$le_petite_url_permalink.'" />';
+		}
+	
+	}
+}
+
+function le_petite_url_shortlink_header()
+{
+	if(is_page() || is_single()) {
+		global $post;
+	
+		global $wp_query;
+		global $wpdb;
+		global $petite_table;
+		
+		$le_petite_url_permalink_domain = get_option('le_petite_url_permalink_domain');
+		$le_petite_url_domain_custom = get_option('le_petite_url_domain_custom');
+		
+		if($le_petite_url_permalink_domain != "custom" )
+		{
+			$blogurl = get_bloginfo('siteurl');
+		}
+		else
+		{
+			$blogurl = 'http://'.$le_petite_url_domain_custom;
+		}
+		
+		$url_table = $wpdb->prefix . $petite_table;
+		$post_id = $wp_query->post->ID;
+		$anchor_text = get_option('le_petite_url_link_text');
+	
+		$petite_url = $wpdb->get_var("SELECT petite_url FROM ".$url_table." WHERE post_id = ".$post_id."");
+		if($petite_url != "")
+		{
+			$le_petite_url_permalink = $blogurl;
+			if(get_option('le_petite_url_permalink_prefix') == "custom")
+			{
+				$le_petite_url_permalink = $le_petite_url_permalink . get_option('le_petite_url_permalink_custom');
+			}
+			else
+			{
+				$le_petite_url_permalink = $le_petite_url_permalink . "/";
+			}
+			$le_petite_url_permalink = $le_petite_url_permalink . $petite_url;
+			
+			echo '<link rel="shortlink" href="'.$le_petite_url_permalink.'" />';
 		}
 	
 	}
@@ -336,6 +428,11 @@ function le_petite_url_register()
 {
 	update_option('le_petite_url_registered', "yes");
 	update_option('le_petite_url_registered_on', time());
+}
+
+function le_petite_url_hide_godaddy()
+{
+	update_option('le_petite_url_hide_godaddy', "yes");
 }
 
 // function adapted from http://www.webcheatsheet.com/PHP/get_current_page_url.php
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Plugin Name ===
 Contributors: philnelson
-Version: 1.06
-Plugin URI: http://extrafuture.com/projects/le-petite-url/
-Tags: permalink, urls, mobile, url shortener, url shortening, shorturl, short_url
+Version: 1.5
+Plugin URI: http://extrafuture.com/projects/la-petite-url/
+Tags: permalink, urls, mobile, url shortener, url shortening, shorturl, short_url, shortlink, linking
 Requires at least: 2.5
-Tested up to: 2.8.3
+Tested up to: 2.8.4
 Stable tag: trunk
 Author: Phil Nelson
 Author URI: http://extrafuture.com
@@ -13,9 +13,9 @@ la petite url is an automatic URL shortener, allowing the user to provide simple
 
 == Description ==
 
-la petite url is a personal URL shortener. Using your own Wordpress (2.5+) installation, la petite url allows the user to create shortened, unique, permalinks to their content using a combination of lowercase, uppercase, and numeric characters, which originate from their own domain name. By default la petite url generates a 5-character combination of lowercase letters only, for ease of use in entering on a mobile device or handset.
+la petite url is a personal URL shortener. Using your own Wordpress (2.5+) installation, la petite url allows the user to create shortened, unique, permalinks to their content using a combination of lowercase, uppercase, and numeric characters, which originate from their own domain name. By default la petite url generates a 5-character combination of lowercase letters only, for ease of use in entering on a mobile device or handset. With version 1.5 la petite url allows the user to add a custom, shorter domain for these shortened links.
 
-la petite url also supports <a href="http://sites.google.com/a/snaplog.com/wiki/short_url">shorturl auto discovery</a>.
+la petite url also supports <a href="http://sites.google.com/a/snaplog.com/wiki/short_url" title="read more about shorturl">shorturl auto discovery</a> and <a href="http://microformats.org/wiki/rel-shortlink" title="Read more about shortlink">rel="shortlink"</a>.
 
 == Installation ==
 
