00b4220381d09b9e04bd0563cfdb4078334b6328
philnelson <philnelson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-10 17:11:06 +0000

    Version 2.0 launch?
    git-svn-id: https://plugins.svn.wordpress.org/le-petite-url/trunk@239399 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ la-petite-url.js
@@ -0,0 +1,33 @@
+jQuery(document).ready( function($) {
+	
+	$('input[name=le_petite_url_permalink_domain]').change(function(event) {
+		if($('input[name=le_petite_url_permalink_domain]:checked').val() == 'custom')
+		{
+			$('#custom-domain-input').slideDown();
+		}
+		
+		if($('input[name=le_petite_url_permalink_domain]:checked').val() == 'default')
+		{
+			$('#custom-domain-input').slideUp();
+		}
+		
+		$(this).next('button').html('hello');
+		
+	});
+	
+	$('input[name=le_petite_url_permalink_prefix]').change(function(event) {
+		if($('input[name=le_petite_url_permalink_prefix]:checked').val() == 'custom')
+		{
+			$('#custom-prefix-input').slideDown();
+		}
+		
+		if($('input[name=le_petite_url_permalink_prefix]:checked').val() == 'default')
+		{
+			$('#custom-prefix-input').slideUp();
+		}
+	});
+	
+	
+	
+
+});
